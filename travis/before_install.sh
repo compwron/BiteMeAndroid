4598705ebad3c4215ac07e88a79dc0bd884c4c39
Pete Hodgson <git@thepete.net>
2013-04-02 20:40:15 -0700

    move back into project root before running travis/before_install.sh

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -3,6 +3,8 @@ set -e
 
 PROJECT_ROOT="`dirname \"$0\"`/.."
 
+cd $PROJECT_ROOT
+
 sudo apt-get update -qq
 if [ `uname -m` = x86_64 ]; then sudo apt-get install -qq --force-yes libgd2-xpm ia32-libs ia32-libs-multiarch; fi
 wget http://dl.google.com/android/android-sdk_r21.0.1-linux.tgz
