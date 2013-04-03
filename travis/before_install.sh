65ea89a5175b5265a8e6610ec303254bc9f62f16
Pete Hodgson <git@thepete.net>
2013-04-02 20:02:46 -0700

    forward slash != backward slash

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -7,7 +7,7 @@ sudo apt-get update -qq
 if [ `uname -m` = x86_64 ]; then sudo apt-get install -qq --force-yes libgd2-xpm ia32-libs ia32-libs-multiarch; fi
 wget http://dl.google.com/android/android-sdk_r21.0.1-linux.tgz
 tar -xzf android-sdk_r21.0.1-linux.tgz
-export ANDROID_HOME=$PROJECT_ROOT\android-sdk-linux
+export ANDROID_HOME=$PROJECT_ROOT/android-sdk-linux
 export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools
 android update sdk --filter platform-tools,android-16,extra-android-support,android-17,sysimg-17,extra-google-google_play_services --no-ui --force
 cp vendor/GmsMvn/gms-mvn-install.sh android-sdk-linux/extras/google/google_play_services/libproject/google-play-services_lib/ 
