f3cc31bd8599d94e0d6bb6e210942b9d9e412a01
Toby Clemson <tobyclemson@gmail.com>
2013-04-03 01:19:18 -0700

    Making before_install be install so prevent a mvn install from running and setting up ANDROID_HOME and PATH before each build stage.

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -e
+
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
 cd $PROJECT_ROOT
 
+export ANDROID_HOME=$PROJECT_ROOT/android-sdk-linux
+export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 sleep 3 # give xvfb some time to start 
--- travis/script.sh
+++ travis/script.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -e
+
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
 cd $PROJECT_ROOT
 
 export ANDROID_HOME=$PROJECT_ROOT/android-sdk-linux
+export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools
 
 mvn install
 adb kill-server
