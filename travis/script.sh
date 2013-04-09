86801ceaf8b47f292cdc82bb1df4fd26a903c748
Lina Qu <lqu@thoughtworks.com>
2013-04-09 14:08:21 -0700

    fix build due to apk name change

--- travis/script.sh
+++ travis/script.sh
@@ -12,5 +12,5 @@ mvn install
 adb kill-server
 adb start-server
 adb devices
-adb install -r target/bitemoi-1.0-SNAPSHOT.apk
-calabash-android run target/bitemoi-1.0-SNAPSHOT.apk
+adb install -r target/bitemoi-1.0.apk
+calabash-android run target/bitemoi-1.0.apk
